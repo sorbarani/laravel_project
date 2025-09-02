972107879ef9bba049d98c61eb9420f411535e0d
sorbarani <soroushbarani78@gmail.com>
2025-09-02 11:29:44 +0330

    feat(post):add new controllers

--- app/Http/Controllers/post.php
+++ app/Http/Controllers/post.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class post extends Controller
 {
     public function index()
     {
-        dd(request());
+        $products = Product::all();
+        return view('index', compact('products'));
     }
 
     public function store(Request $request)
@@ -16,8 +18,20 @@ class post extends Controller
         //store the user input
         // $value = request()->product;
         $array =  request()->all();
-        var_dump($array);
+        // var_dump($array);
+        
+        echo "<br>";
+        echo "name :".$array["product"]."<br>";
+        echo "price :".$array["price"]."<br>";
+        echo "date :".$array["date"]."<br>";
+        echo "producer :".$array["producer"]."<br>";
 
+        Product::create([
+        'name'=>$array['product'],
+        'price'=>$array['price'],
+        'created_at'=>$array['date'],
+        'producer'=>$array['producer'],
+        ]);
     }
     
 }
