ca51da52690775a752c0cf7271027aef9eea3ed5
sorbarani <soroushbarani78@gmail.com>
2025-09-02 11:35:41 +0330

    feat(home):create form product info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,18 @@
 
     <form action="/form-submit" method="post">
         @CSRF
-        <lablel for="product">Product</lablel>
+        <lablel for="product">Name of product</lablel>
         <input type="text" id="product" name="product" placeholder="Type your product name">
+        <br>
+        <label for="price">Price</label>
+        <input type="number" id="price" name="price" min="0" step="0.01" placeholder="Enter price">
+        <br>
+        <label for="date">Date</label>
+        <input type="date" id="date" name="date" placeholder="Enter date">
+        <br>
+        <lablel for="producer">Name of product</lablel>
+        <input type="text" id="producer" name="producer" placeholder="Company">
+        <br>
         <button type="submit">Submit</butto>
     </form>
 </body>
