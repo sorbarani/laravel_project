bbd48ea91160305c9b82cbfc4d912c1971607049
sorbarani <soroushbarani78@gmail.com>
2025-09-02 11:57:07 +0330

    fix(brands): syntax error in $brand

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -7,18 +7,18 @@
 
 <body>
     <h1>All Products</h1>
-    <!-- <table border="1" cellpadding="5">
+    <table border="1" cellpadding="5">
         <tr>
             <th>ID</th>
             <th>Name</th>
         </tr>
-        @foreach($products as $product)
+        @foreach($brands as $brand)
         <tr>
-            <td>{{ $product->id }}</td>
-            <td>{{ $product->name }}</td>
+            <td>{{ $brand->id }}</td>
+            <td>{{ $brand->name }}</td>
         </tr>
         @endforeach
-    </table> -->
+    </table>
 </body>
 
 </html>
