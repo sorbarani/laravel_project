960048b4b0b4f29bb07851857652d4dd06efc987
sorbarani <soroushbarani78@gmail.com>
2025-09-02 12:16:27 +0330

    feat(BrandController):add index function

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Brand;
 
 class BrandController extends Controller
 {
-    //
+    public function index()
+    {
+        $brands = Brand::all();
+        return view('brands', compact('brands'));
+    }
 }
