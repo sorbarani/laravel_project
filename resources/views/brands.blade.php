1e956ad5398f1f4db33d2433628d54dd827a699d
sorbarani <soroushbarani78@gmail.com>
2025-09-02 11:43:21 +0330

    feat(brands):add a view to show brands

---
+++ resources/views/brands.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Products</title>
+</head>
+
+<body>
+    <h1>All Products</h1>
+    <!-- <table border="1" cellpadding="5">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+        </tr>
+        @foreach($products as $product)
+        <tr>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->name }}</td>
+        </tr>
+        @endforeach
+    </table> -->
+</body>
+
+</html>
