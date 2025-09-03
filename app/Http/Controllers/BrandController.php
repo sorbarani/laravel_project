96877faae2ae9c3b46d75ea6df9f65f52a983bbc
sorbarani <soroushbarani78@gmail.com>
2025-09-03 16:02:52 +0330

    feat(BrandController):add delete,store,update methods

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Brand;
 
@@ -11,5 +12,40 @@ class BrandController extends Controller
     {
         $brands = Brand::all();
         return view('brands', compact('brands'));
+    }
+
+    public function store(Request $request)
+    {
+        //store the user input
+        $array =  request();
+        echo "<br>";
+        echo "name :" . $array["name"] ."Created". "<br>";
+
+        Brand::create([
+            'name' => $array['name'],
+        ]);
+    }
+
+    public function delete(Request $request)
+    {
+        $array = request();
+
+        Brand::where('name', $array['name'])->delete();
+        echo $array['name']."Deleted";
+    }
+
+    public function update()
+    {
+        $array = request();
+
+        echo "Previous name: ".$array['name']."<br>";
+        echo "New name: ".$array['new_name']."<br>";
+
+        Brand::where('name', $array['name'])
+            ->update(['name' => $array['new_name']]);
+
+        echo "<br>";
+        echo "Updated";
+        
     }
 }
