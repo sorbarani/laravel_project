864869f649cdcf12ecdfdc28d1b1cb35ae29c598
sorbarani <soroushbarani78@gmail.com>
2025-09-10 15:26:31 +0330

    fix(brand-update):apply red color to error message.

--- resources/views/brand-update.blade.php
+++ resources/views/brand-update.blade.php
@@ -10,7 +10,7 @@
 <body>
     @if ($errors->any())
     <div class="alert alert-danger">
-        <ul>
+        <ul style="color:red;">
             @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
             @endforeach
