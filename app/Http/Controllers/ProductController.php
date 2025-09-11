8f3372bf5679d6d6aefa26673dd4b5770ce95516
sorbarani <soroushbarani78@gmail.com>
2025-09-11 19:19:12 +0330

    feat(ProductController):add validation to input before inset to database.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-class post extends Controller
+class ProductController extends Controller
 {
     public function index()
     {
@@ -21,17 +21,19 @@ class post extends Controller
         $array =  request()->all();
         // var_dump($array);
         
+        $validated = $request->validate([
+            'name' => 'required|string|min:1|max:255',
+            'price' => 'required|numeric|min:0',
+            'date' => 'required|date',
+            'brand_id' => 'required|exists:brand,id',
+        ]);
+
         echo "<br>";
-        echo "name :".$array["product"]."<br>";
+        echo "name :".$array["name"]."<br>";
         echo "price :".$array["price"]."<br>";
         echo "date :".$array["date"]."<br>";
         echo "brand_id:".$array["brand_id"]."<br>";
 
-        Product::create([
-        'name'=>$array['product'],
-        'price'=>$array['price'],
-        'created_at'=>$array['date'],
-        'brand_id'=>$array['brand_id']
-        ]);
+        Product::create($validated);
     }
 }
