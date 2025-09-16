17dda8caef58cd969ab54f00afa64ab389a3f623
sorbarani <soroushbarani78@gmail.com>
2025-09-16 09:58:00 +0330

    feat(Order):add Order model

---
+++ app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'id',
+        'user_id',
+    	'status',
+        'total_amount',
+    	'delivery_amount',
+    	'delivery_option_id',
+    	'offer_amount',
+    	'paying_amount',
+     	'payment_status',
+    	'address_id',
+    	'shipment_code',
+    	'order_type' ,
+        'description', 
+    	'slug',
+     	'deleted_at',
+     	'created_at'
+	];
+}
