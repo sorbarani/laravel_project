0a8ee9dd02de0b65e5643a9cb8e3d90b43b928dc
sorbarani <soroushbarani78@gmail.com>
2025-09-16 09:58:18 +0330

    feat(add):add OrderProduct model.

---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderProduct extends Model
+{
+    use HasFactory;
+
+    protected $table = 'order_product';
+    protected $fillable = [
+        'id',
+        'order_id',
+        'product_id',
+        'sub_product',
+        'offer_id',
+        'offer_id_value',
+        'offer_on_product',
+        'price',
+        'quantity ',
+        'subtotal '	,
+        'created_at', 
+        'updated_at',
+    ];
+}
