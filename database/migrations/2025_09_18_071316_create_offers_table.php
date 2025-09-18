66e3e8a3698eab47746a0b33b219bf912f99c4d1
sorbarani <soroushbarani78@gmail.com>
2025-09-18 11:17:13 +0330

    feat(2025_09_18_071316_create_offers_table.php):add new offers table to the database.

---
+++ database/migrations/2025_09_18_071316_create_offers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('value');
+            $table->decimal('base_price', 10, 2);
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->unsignedTinyInteger('percent');
+            $table->unsignedInteger('count')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
