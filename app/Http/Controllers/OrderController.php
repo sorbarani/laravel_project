14db270836dec0ccce687a2d26a4825d7e99b582
sorbarani <soroushbarani78@gmail.com>
2025-09-18 10:36:10 +0330

    feat(OrderController):add OrderProduct creat to controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Str;
 use App\Models\Product;
 use App\Models\Order;
+use App\Models\OrderProduct;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -26,10 +27,11 @@ class OrderController extends Controller
             'quantity' => 'required|integer|min:1|max:10'
         ]);
 
-        Order::create([
-            'user_id' => 1,
+
+        $order = Order::create([
+            'user_id' =>  1,
             'status' => 1,
-            'total_amount' => $this->get_price($request->quantity, $product->price),
+            'total_amount' => 1400,
             'delivery_amount' => 0,
             'delivery_option_id' => null,
             'offer_amount' => 0,
@@ -40,6 +42,18 @@ class OrderController extends Controller
             'order_type' => 0,
             'description' => "",
             'slug' => Str::random(16)
+        ]);
+
+        OrderProduct::create([
+            'order_id' => $order->id,
+            'product_id' => $product->id,
+            'sub_product' => 0,
+            'offer_id' => 0,
+            'offer_id_value'=> 0,
+            'offer_on_product'=>0,
+            'price' => 1200,
+            'quantity'=> $request->quantity,
+            'subtotal'=> $this->get_price($request->quantity, $product->price),
         ]);
 
         echo "Total amount is: ".$this->get_price($request->quantity, $product->price );
