ae85f324831d8bab2028f55af7b2a6764422be62
sorbarani <soroushbarani78@gmail.com>
2025-10-11 11:03:09 +0330

    feat(create_order_items_table):add migration of order_items table.

---
+++ database/migrations/2025_10_11_065623_create_order_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('sub_produc')->nullable();
+            $table->unsignedBigInteger('offer_id')->nullable();
+            $table->bigInteger('offer_id_value')->default(0)->comment('this value came from offer_id value');
+            $table->bigInteger('offer_on_product')->default(0);
+            $table->bigInteger('price');
+            $table->bigInteger('quantity');
+            $table->bigInteger('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
